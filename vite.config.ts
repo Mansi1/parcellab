e24808a77643f07ed338ead348130b63482f31c8
Michael Mannseicher <m.mansi1@gmx.net>
2024-09-24 10:46:42 +0200

    add: first commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import type { PluginOption } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
+import vercel from 'vite-plugin-vercel';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svgr(), react(), vercel(), visualizer() as PluginOption],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+});
