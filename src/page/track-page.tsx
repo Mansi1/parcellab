a6183ee1206c2540f8642c043795f89435a48f3d
Michael Mannseicher <m.mansi1@gmx.net>
2024-09-24 13:10:49 +0200

    add: validation and state logic

--- src/page/track-page.tsx
+++ src/page/track-page.tsx
@@ -1,25 +1,68 @@
-import { FormEvent, FormEventHandler, useState, type FC } from 'react';
+import { FormEvent, ReactNode, useState, type FC } from 'react';
 import Layout from '../components/layout';
 import SquarLogo from '../assets/parcelab-logo-square.svg?react';
 import Button from '../components/button';
+import { getOrder } from '../api';
+import { useNavigate } from 'react-router-dom';
 interface TrackPageProps {}
 type TrackPageState = {
+  status: 'NOT_FOUND' | 'NONE' | 'LOADING' | 'ERROR' | 'INVALID';
   orderNumber: string;
   zipCode: string;
 };
+const STATUS_MAP: Record<TrackPageState['status'], ReactNode> = {
+  NOT_FOUND: (
+    <div className="text-left text-xs text-red-700">Sorry no order found!</div>
+  ),
+  NONE: <div className="text-left text-xs opacity-0">NONE</div>,
+  LOADING: (
+    <div className="text-left text-xs text-gray-700">Loading order...</div>
+  ),
+  ERROR: (
+    <div className="text-left text-xs text-red-700">
+      An unexpected error happend please try again!
+    </div>
+  ),
+  INVALID: (
+    <div className="text-left text-xs text-red-700">Input is invalid</div>
+  ),
+};
+
+console.log('orderNumber', 'AB20221219');
+console.log('zipCode', '60156');
+
 const TrackPage: FC<TrackPageProps> = () => {
+  const navigate = useNavigate();
+
   const [state, setState] = useState<TrackPageState>({
+    status: 'NONE',
     orderNumber: '',
     zipCode: '',
   });
 
-  const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-
-    //validate state
-
-    //redirect to other view
+  const handleFormSubmit = async (evt: FormEvent<HTMLFormElement>) => {
+    try {
+      evt.preventDefault();
+      setState((c) => ({ ...c, status: 'LOADING' }));
+      //validate state
+      if (state.orderNumber?.length < 3 || state.zipCode?.length < 3) {
+        setState((c) => ({ ...c, status: 'INVALID' }));
+        return;
+      }
+      //redirect to other view
+      const order = await getOrder(state.orderNumber, state.zipCode);
+      if (!order) {
+        setState((c) => ({ ...c, status: 'NOT_FOUND' }));
+      } else {
+        navigate(`/order/${order.tracking_number}/zipcode/${order.zip_code}`, {
+          replace: true,
+        });
+      }
+    } catch (e) {
+      setState((c) => ({ ...c, status: 'ERROR' }));
+    }
   };
+
   return (
     <Layout>
       <form
@@ -45,9 +88,16 @@ const TrackPage: FC<TrackPageProps> = () => {
           <input
             value={state.orderNumber}
             onChange={(evt) =>
-              setState((c) => ({ ...c, orderNumber: evt.target.value }))
+              setState((c) => ({
+                ...c,
+                status: 'NONE',
+                orderNumber: evt.target.value,
+              }))
             }
-            className="border-2 rounded-md bg-gray-50 p-2 w-full"
+            className={
+              'border-2 rounded-md bg-gray-50 p-2 w-full' +
+              (state.status === 'INVALID' ? ' border-red-700' : '')
+            }
             name="orderNumber"
           />
         </div>
@@ -61,14 +111,24 @@ const TrackPage: FC<TrackPageProps> = () => {
           <input
             value={state.zipCode}
             onChange={(evt) =>
-              setState((c) => ({ ...c, zipCode: evt.target.value }))
+              setState((c) => ({
+                ...c,
+                status: 'NONE',
+                zipCode: evt.target.value,
+              }))
             }
-            className="border-2 rounded-md bg-gray-50 p-2 w-full"
+            className={
+              'border-2 rounded-md bg-gray-50 p-2 w-full' +
+              (state.status === 'INVALID' ? ' border-red-700' : '')
+            }
             name="zipcode"
           />
         </div>
         <hr></hr>
-        <Button type="submit">Track</Button>
+        {STATUS_MAP[state.status]}
+        <Button disabled={state.status === 'LOADING'} type="submit">
+          Track
+        </Button>
       </form>
     </Layout>
   );
